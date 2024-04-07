1521f511d91ed7e0dc8b9bf6cb3f217504a82392
Ankush Srivastava <ankushsri007@gmail.com>
2024-04-08 00:13:07 +0530

    completed reg and modles module

--- dcrm/git files/views.py
+++ dcrm/git files/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
+
 
 # Create your views here.
 def home(request):
@@ -32,5 +34,24 @@ def logout_user(request):
    return redirect('home')
 
 
+def register_user(request):
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and Login
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=authenticate(usrname=username,password=password)
+            login(request,user)
+            messages.success(request,"You have been successfully registered!Welcome!")
+            return redirect('home')
+        
+
+    else:
+        form=SignUpForm()
+        return render(request,'register.html',{'form':form})
+    
+    return render(request,'register.html',{'form':form})
 
 
