1c2879564a32d05329c3750f7a80937934de89d0
Ankush Srivastava <ankushsri007@gmail.com>
2024-04-13 21:27:29 +0530

    pushed again

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignUpForm
 
-#solved views problems
+#solved views problems 
 # Create your views here.
 def home(request):
     #login 
