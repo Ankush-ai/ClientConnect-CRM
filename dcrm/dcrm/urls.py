e0b7ce67436f8f85c897d6cf3e0a22f642a5e3e5
Anubhuti Srivastava <anubhutisrivastava312@gmail.com>
2024-04-14 19:55:52 +0530

    Resolved Conflicts

--- dcrm/dcrm/urls.py
+++ dcrm/dcrm/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include 
-from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('website.urls'))
-    path('', include('website.urls')),
+    path('',include('website.urls')),
 ]
